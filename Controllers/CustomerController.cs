2d9463e6cad37346c3ec492e990856443b05f6e3
hh <117482328+pgramr@users.noreply.github.com>
2022-12-15 21:53:21 +0100

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,96 @@
+﻿using Contentful.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace DevOpsContentful.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly IContentfulClient _client;
+        private readonly HttpClient httpClient1 = new HttpClient();
+        public ContentfulManagementClient GetClient()
+        {
+            var client = new ContentfulManagementClient(httpClient1, "CFPAT-jKh-8Ow0RWjRyEK39dgunPtCKPG8wa-0cPZfoho9IQw", "qui5qc0w6jjg");
+            return client;
+        }
+        public CustomersController(IContentfulClient client)
+        {
+            _client = client;
+        }
+        // GET: CustomerController
+        public ActionResult View1()
+        {
+            return View();
+        }
+
+        // GET: CustomerController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CustomerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Create));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Create));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Create));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
