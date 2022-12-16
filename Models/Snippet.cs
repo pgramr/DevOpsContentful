14f61377b48fc15ca75bef0decfa06b1b95e4c52
hh <117482328+pgramr@users.noreply.github.com>
2022-12-17 00:30:53 +0100

    f

--- Models/Snippet.cs
+++ Models/Snippet.cs
@@ -9,39 +9,17 @@ namespace DevOpsContentful.Models
         [JsonProperty("sys")]
         public SystemProperties SystemProperties { get; set; }
 
-        //
-        // Summary:
-        //     The description of the asset.
         public string Description { get; set; }
 
-        //
-        // Summary:
-        //     The title of the asset.
         public string Title { get; set; }
 
-        //
-        // Summary:
-        //     Encapsulates information about the binary file of the asset.
         public File File { get; set; }
 
-        //
-        // Summary:
-        //     The titles of the asset per locale.
         public Dictionary<string, string> TitleLocalized { get; set; }
 
-        //
-        // Summary:
-        //     The descriptions of the asset per locale.
         public Dictionary<string, string> DescriptionLocalized { get; set; }
 
-        //
-        // Summary:
-        //     Information about the file in respective language.
         public Dictionary<string, File> FilesLocalized { get; set; }
 
-        ////
-        //// Summary:
-        ////     The system managed metadata of an entry.
-        //public ContentfulMetadata Metadata { get; set; }
     }
 }
